9b6d6ebcdab18136fb5eac44812e926ba8a48953
Hirruá <hirrua.silva20@gmail.com>
2026-02-12 20:15:57 -0300

    start race condition

--- src/exemplos_threads/race_condition/ProdutorConsumidor.java
+++ src/exemplos_threads/race_condition/ProdutorConsumidor.java
@@ -11,56 +11,61 @@ public class ProdutorConsumidor {
     private static boolean produzindo = true;
     private static boolean consumindo = true;
 
-    Thread produtor = new Thread(() -> {
-        while (true) {
-            try {
-                processamento();
-                if (produzindo) {
-                    int num = new Random().nextInt(100);
-                    LISTA.add(num);
-                    if (LISTA.size() == 5) {
-                        System.out.println("Pausando consumidor");
-                        produzindo = false;
+    public static void main (String args[]) {
+        Thread produtor = new Thread(() -> {
+            while (true) {
+                try {
+                    processamento();
+                    if (produzindo) {
+                        int num = new Random().nextInt(100);
+                        LISTA.add(num);
+                        if (LISTA.size() == 5) {
+                            System.out.println("Pausando consumidor");
+                            produzindo = false;
+                        }
+                        if (LISTA.size() == 1) {
+                            System.out.println("Iniciando consumidor");
+                            consumindo = true;
+                        }
+                        else {
+                            System.out.println("!!!!!!!!!!!Produtor domindo!!!!!!!!!!");
+                        }
                     }
-                    if (LISTA.size() == 1) {
-                        System.out.println("Iniciando consumidor");
-                        consumindo = true;
-                    }
-                    else {
-                        System.out.println("!!!!!!!!!!!Produtor domindo!!!!!!!!!!");
-                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
-        }
-    });
+        });
 
-    Thread consumidor = new Thread(() -> {
-        while (true) {
-            try {
-                processamento();
-                if (consumindo) {
-                    System.out.println("Consumindo");
-                    Optional<Integer> numero = LISTA.stream().findFirst();
-                    numero.ifPresent(LISTA::remove);
-                    if (LISTA.size() == 0) {
-                        System.out.println("Pausando consumidor");
-                        consumindo = false;
+        Thread consumidor = new Thread(() -> {
+            while (true) {
+                try {
+                    processamento();
+                    if (consumindo) {
+                        System.out.println("Consumindo");
+                        Optional<Integer> numero = LISTA.stream().findFirst();
+                        numero.ifPresent(LISTA::remove);
+                        if (LISTA.size() == 0) {
+                            System.out.println("Pausando consumidor");
+                            consumindo = false;
+                        }
+                        if (LISTA.size() == 4) {
+                            System.out.println("Iniciando produtor");
+                            produzindo = true;
+                        }
+                        else {
+                            System.out.println("!!!!!!!Consumidor dormindo!!!!!!");
+                        }
                     }
-                    if (LISTA.size() == 4) {
-                        System.out.println("Iniciando produtor");
-                        produzindo = true;
-                    }
-                    else {
-                        System.out.println("!!!!!!!Consumidor dormindo!!!!!!");
-                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
-        }
-    });
+        });
+
+        produtor.start();
+        consumidor.start();
+    }
 
     private static final void processamento() {
         int tmp = new Random().nextInt(10);
